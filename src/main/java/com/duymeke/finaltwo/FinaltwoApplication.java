f5d869ef9708ed9d8251d004a703dd2c23dfb2ee
Dinmukhamed <suleimenovdimash7@gmail.com>
2026-01-13 22:41:50 +0500

    first commit

---
+++ src/main/java/com/duymeke/finaltwo/FinaltwoApplication.java
@@ -0,0 +1,13 @@
+package com.duymeke.finaltwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinaltwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinaltwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duymeke/finaltwo/FinaltwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duymeke.finaltwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaltwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
