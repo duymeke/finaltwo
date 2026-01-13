b0079ea58b805b9896ec8509252aad4181910900
Dinmukhamed <suleimenovdimash7@gmail.com>
2026-01-14 02:50:24 +0500

    Refactoring

--- src/main/java/com/duymeke/finaltwo/entity/Account.java
+++ src/main/java/com/duymeke/finaltwo/entity/Account.java
@@ -4,6 +4,10 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 import java.util.List;
 
 @Entity
